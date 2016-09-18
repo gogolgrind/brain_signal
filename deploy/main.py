425edf3e9f6137c9b657e77e2a63a9e4a8e5e853
Artem Kondyukov <artemkondyukov@gmail.com>
2016-09-18 21:46:46 +0300

    fixes

--- deploy/main.py
+++ deploy/main.py
@@ -2,9 +2,6 @@ import os
 import sys
 from PyQt4.QtGui import *
 
-# os.environ["PYTHONPATH"] = "F:\\YandexDisk\\brain_company"
-# os.environ["PYTHONBUFFERED"] = "1"	
-
 from emg.app import *
 
 
@@ -16,7 +13,7 @@ def window():
     sys.exit(app.exec_())
     # del window, app
     # sys.exit(0)
-	
+
+
 if __name__ == '__main__':
     window()
-    print(123)
